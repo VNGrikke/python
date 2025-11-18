59bb7dd49ba93236f94efd0727b644ed978e5fbc
VNGrikke <enlove2005@gmail.com>
2025-11-18 20:40:04 +0700

    update b7

--- SS02/bt7.py
+++ SS02/bt7.py
@@ -1,6 +1,25 @@
 a = int(input("Nhập vào 1 số nguyên a: "))
 b = int(input("Nhập vào 1 số nguyên b: "))
 
-start = a if a % 2 == 0 else a + 1
-array = list(range(start, b + 1, 2))
-print(array)
+if a > b: 
+    a, b = b, a     
+found_primes = False
+for n in range(a, b + 1):
+    if n <= 1:
+        continue
+    is_prime = True
+
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            is_prime = False
+            break  
+        i += 1
+        
+    if is_prime:
+        print(n, end=" ")
+        found_primes = True
+        
+if not found_primes:
+    print("Không có số nguyên tố nào trong khoảng này.", end="")
+    
